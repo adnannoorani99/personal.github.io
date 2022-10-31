cb60388ce865654bf0a6bc84f8bf4c17aae77799
Unknown <60357876+unknown.noreply@github.com>
2022-10-31 16:12:14 +0530

    personal V1

---
+++ footer.php
@@ -0,0 +1,9 @@
+    <div class="footer">
+        <div class="container" align="middle">
+<!--            <h4 style="display: inline;">Designed & developed by</h4><h2 style="display: inline;color: green;">  ADNAN AHMED</h2>-->
+                <h3 style="color: brown;" align="middle">Designed & Developed by</h3>
+                <h2 style="color: green;" align="middle">ADNAN AHMED</h2>
+                <h5 style="color: blue;" align="middle">adnannoorani99@gmail.com</h5>
+                <h5 style="color: blue;" align="middle">8446696915</h5>
+        </div>
+    </div>
